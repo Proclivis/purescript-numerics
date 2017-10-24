f5098e4da8afff609c371476418a6f24f5687e8b
Aidan Delaney <aidan@ontologyengineering.org>
2017-10-25 11:54:47 +1200

    Added `sqrt` to `Num` typeclass as it's part of the Haskell `Num` typeclass

--- src/Data/Num.purs
+++ src/Data/Num.purs
@@ -1,7 +1,7 @@
 -- | A `Num` typeclass.
 module Data.Num
   ( class Num
-  , negate, abs, signum
+  , negate, abs, signum, sqrt
   , fromBigInt
   ) where
 
@@ -11,12 +11,14 @@ import Data.Int as I
 import Data.Maybe (fromMaybe)
 import Data.Ord as O
 import Data.Ring as R
+import Math as M
        
 -- | The `Num` class represents general numbers, similar to the Haskell version.
 class (Eq a, Semiring a, Ring a, CommutativeRing a) <= Num a where
     negate :: a -> a
     abs :: a -> a
     signum :: a -> a
+    sqrt :: a -> a
     fromBigInt :: BI.BigInt -> a
 
 -- | `Num` instance for `Int`.
@@ -24,6 +26,8 @@ instance numInt :: Num Int where
     negate = R.negate
     abs = O.abs
     signum = O.signum
+    -- The `fromMaybe` call should never return the default value in this usage
+    sqrt = fromMaybe (0 :: Int) <<< I.fromNumber <<< M.sqrt <<< I.toNumber
     fromBigInt bi = fromMaybe 0 $ (I.fromNumber <<< BI.toNumber) bi
    
 -- | `Num` instance for `Number`.
@@ -31,6 +35,7 @@ instance numNumber :: Num Number where
     negate = R.negate
     abs = O.abs
     signum = O.signum
+    sqrt = M.sqrt
     fromBigInt bi = BI.toNumber bi
 
 -- | `Num` instance for `BigInt`.
@@ -38,4 +43,6 @@ instance numBigInt :: Num BI.BigInt where
     negate = R.negate
     abs = O.abs
     signum = O.signum
+    -- FIXME: Implement sqrt on BigInt.  This implementation looses precision.
+    sqrt = BI.fromInt <<< fromMaybe (0 :: Int) <<< I.fromNumber <<< M.sqrt <<< BI.toNumber
     fromBigInt = id
